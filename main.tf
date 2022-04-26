ff7efa72eff7c1b671fa1ac364b5557a25d9bfaa
Tobang1 <akinolajoseph422@gmail.com>
2022-04-26 19:21:20 +0100

    initial

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 # --- root/main.tf ---
 
 locals {
-  vpc_cidr = "10.123.0"
+  vpc_cidr = "10.123.0.0/16"
 }
 
 module "networking" {
   source           = "./networking"
-  vpc_cidr         = "10.123.0.0/16"
+  vpc_cidr = local.vpc_cidr
   public_sn_count  = 2
   private_sn_count = 3
   max_subnets      = 20
-  public_cidrs     = [for i in range(2, 225, 2) : cidrsubnet("10.123.0.0/16", 8, i)] #["10.123.2.0/24", "10.123.4.0/24" ]
-  private_cidrs    = [for i in range(1, 225, 2) : cidrsubnet("10.123.0.0/16", 8, i)] #["10.123.1.0/24", "10.123.3.0/24", "10.123.5.0/24"]
+  public_cidrs     = [for i in range(2, 225, 2) : cidrsubnet(local.vpc_cidr, 8, i)] #["10.123.2.0/24", "10.123.4.0/24" ]
+  private_cidrs    = [for i in range(1, 225, 2) : cidrsubnet(local.vpc_cidr, 8, i)] #["10.123.1.0/24", "10.123.3.0/24", "10.123.5.0/24"]
 }
