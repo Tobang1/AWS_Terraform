403228e9c1c94e43c554bb20b12f9b8ee3a4e001
Tobang1 <akinolajoseph422@gmail.com>
2022-05-02 10:16:16 +0100

    Target-group update

--- Loadbalancing/variables.tf
+++ Loadbalancing/variables.tf
@@ -1,4 +1,11 @@
 # ----- loadbalancing/variable.tf ----
 
 variable "public_sg" {}
-variable "public_subnets"{}
+variable "public_subnets" {}
+variable "tg_port" {}
+variable "tg_protocol" {}
+variable "lb_healthy_threshold" {}
+variable "lb_unhealthy_threshold" {}
+variable "lb_timeout" {}
+variable "lb_interval" {}
+variable "vpc_id" {}
